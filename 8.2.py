1624f047d45bee8667bc841dbf045629fab87859
alex <alexzhelyapov1>
2022-05-05 17:21:08 +0300

    all

---
+++ 8.2.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+k = 3.3/255
+
+data = np.loadtxt ('data.txt')
+data *= k
+settings = np.loadtxt ('settings.txt')
+
+charge = settings[0]
+finish = settings[1]
+dt = settings[0]
+dv = settings[1]
+
+time = np.linspace (0, finish, len(data))
+
+x_axes = np.linspace (0, finish, 10)
+y_axes = np.linspace(0, data.max(), 10)
+
+plt.ylim([0, data.max() * 1.1])
+plt.xlim([0, finish])
+
+plt.grid()
+
+
+
+plt.plot(time, data, c='blue', linewidth=1, label = 'V(t)')
+
+    
+plt.text(charge + 5, data.max() - 0.5, "charging time: {:.5f} с".format(charge))
+plt.text(charge + 5, data.max() - 0.6, "discharge time:  {:.5f} с".format(finish - charge))
+plt.xlabel(r'Время, $c$',    wrap=True)
+plt.ylabel(r'Напряжение, $V$', wrap=True)
+plt.scatter (settings, data, c="red")
+
+plt.legend()
+plt.title('Процесс заряда и разряда конденсатора в RC-цепи', wrap=True)
+plt.savefig ('graph.png')
+plt.savefig ('graph.svg')
